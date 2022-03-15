82ebb962bcfe1d9eea69b7fae80ae37f6fde9f13
Mingyang Han <mingyang_han@esri.com>
2022-03-15 16:34:23 +0800

    fix: update config and sample text

--- index.py
+++ index.py
@@ -1,18 +1,16 @@
 import spacy
 
 # text form geonet https://community.esri.com/t5/arcgis-experience-builder-questions/
-text = "I have set up an Edit widget in my Experience Builder app. Before the user selects a feature to edit, the widget displays a title at the top, which is taken from the name I have given the widget. However when the user selects a feature, that title changes to the word \"undefined\". I can't figure out where it is getting \"undefined\" from or what to do to fix it. I've tried using both customised settings and the web map settings for the data source, and I\'ve tried changing things in the web map pop-ups and form builder, but none of those make any difference."
+text = "I have set up an Edit widget in my Experience Builder app (an ESRI product)."
 nlp = spacy.load('en_core_web_lg')
 doc = nlp(text)
 print("\nEntities found before training:")
 for ent in doc.ents:
-    # if ent.label_=='PRODUCT':
     print(ent.label_, ent.text)
 
-nlp = spacy.load('./training/model-best')
+nlp = spacy.load('./training/model-last')
 doc = nlp(text)
 
 print("\nEntities found after training:")
 for ent in doc.ents:
-    # if ent.label_=='PRODUCT':
     print(ent.label_, ent.text)
